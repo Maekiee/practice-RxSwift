8da8dd93c58cc5bb8c3876d9d3ada5b136887563
Maekiee <pdwssf@gmail.com>
2025-08-22 09:31:12 +0900

    deatil view 추가

---
+++ RxPractice/DetailViewController/DetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  DetailViewController.swift
+//  RxPractice
+//
+//  Created by 박도원 on 8/22/25.
+//
+
+import UIKit
+
+class DetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
